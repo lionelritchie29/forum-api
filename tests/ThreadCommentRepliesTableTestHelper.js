62c1d6da5aaa21468a29a34fb57c369bd79eb2dd
lionelritchie29 <lionelrtchieee@gmail.com>
2022-04-15 11:18:36 +0700

    feat: add some reply table helper

--- tests/ThreadCommentRepliesTableTestHelper.js
+++ tests/ThreadCommentRepliesTableTestHelper.js
@@ -2,6 +2,30 @@
 const pool = require('../src/Infrastructures/database/postgres/pool');
 
 const ThreadCommentRepliesTableTestHelper = {
+  async addReply({
+    id = 'reply-123',
+    commentId = 'comment-123',
+    userId = 'user-123',
+    content = 'content',
+  }) {
+    const query = {
+      text: 'INSERT * FROM thread_comment_replies VALUES ($1, $2, $3, $4, $5)',
+      values: [id, content, commentId, userId, new Date()],
+    };
+
+    await pool.query(query);
+  },
+
+  async getReply(id) {
+    const query = {
+      text: 'SELECT * FROM thread_comment_replies WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rowCount > 0 ? result.rows[0] : null;
+  },
+
   async cleanTable() {
     const query = {
       text: 'DELETE FROM thread_comment_replies WHERE 1 = 1',
