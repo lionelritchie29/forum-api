590c15b46630172bea0307537f46c3b01865fd4d
lionelritchie29 <lionelrtchieee@gmail.com>
2022-04-16 23:52:35 +0700

    fix: remove delete comment use case dependencies to db

--- src/Applications/use_case/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -1,30 +1,15 @@
 const pool = require('../../../Infrastructures/database/postgres/pool');
-const ThreadCommentsTableTestHelper = require('../../../../tests/ThreadCommentsTableTestHelper');
-const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper');
-const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
 const DeleteCommentUseCase = require('../DeleteCommentUseCase');
 const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
 const ThreadCommentRepository = require('../../../Domains/threads/ThreadCommentRepository');
 
 describe('DeleteCommentUseCase', () => {
-  afterEach(() => {
-    ThreadCommentsTableTestHelper.cleanTable();
-    ThreadsTableTestHelper.cleanTable();
-    UsersTableTestHelper.cleanTable();
-  });
-
   afterAll(async () => {
     await pool.end();
   });
 
   it('should perform delete comment action correctly', async () => {
-    await UsersTableTestHelper.addUser({ id: 'user-123' });
-    await ThreadsTableTestHelper.addThread({ id: 'thread-123', userId: 'user-123' });
-    await ThreadCommentsTableTestHelper.addComment({
-      id: 'comment-123',
-      threadId: 'thread-123',
-      userId: 'user-123',
-    });
+    const [commentId, userId, threadId] = ['comment-123', 'user-123', 'thread-123'];
 
     const threadRepo = new ThreadRepository();
     threadRepo.verifyThread = jest.fn().mockImplementation(() => Promise.resolve);
@@ -38,15 +23,11 @@ describe('DeleteCommentUseCase', () => {
       threadRepository: threadRepo,
       commentRepository: commentRepo,
     });
-    const deleteStatus = await deleteCommentUseCase.execute(
-      'comment-123',
-      'user-123',
-      'thread-123',
-    );
+    const deleteStatus = await deleteCommentUseCase.execute(commentId, userId, threadId);
 
-    expect(threadRepo.verifyThread).toBeCalledWith('thread-123');
-    expect(commentRepo.verifyComment).toBeCalledWith('comment-123');
-    expect(commentRepo.verifyCommentOwner).toBeCalledWith('comment-123', 'user-123');
+    expect(threadRepo.verifyThread).toBeCalledWith(threadId);
+    expect(commentRepo.verifyComment).toBeCalledWith(commentId);
+    expect(commentRepo.verifyCommentOwner).toBeCalledWith(commentId, userId);
     expect(deleteStatus).toEqual(true);
   });
 });
